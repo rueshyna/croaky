d8662cec714f1620dd5daaeead17e8a1b0e96304
Rueshyna <rueshyna(XD)gmail.com>
2012-09-14 16:32:42 +0800

    remove autoload builder.er

--- lib/croaky.rb
+++ lib/croaky.rb
@@ -1,7 +1,6 @@
 require "croaky/version"
 
 module Croaky
-  autoload :Builer, 'croaky/builer'
   autoload :ParseOption, 'croaky/parse_sendfax_option.rb'
   autoload :OptionSet, 'croaky/option_set.rb'
 end
