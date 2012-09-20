026f7bc95cc5014b35e85fd96b575b8df55137b4
Rueshyna <rueshyna(XD)gmail.com>
2012-09-20 19:45:56 +0800

    change :file into facsimile

--- spec/parse_sendfax_option_spec.rb
+++ spec/parse_sendfax_option_spec.rb
@@ -4,6 +4,6 @@ describe Croaky::ParseSendfaxOption do
   it "should parse string option" do
     croaky = Croaky::OptionSet.new
     opts = "-d 82096111 test.ps"
-    Croaky::ParseSendfaxOption.parse(croaky,opts.split).options_status.should == ({:d => ["82096111"], :n => false, :P => "", :file => ["test.ps"]})
+    Croaky::ParseSendfaxOption.parse(croaky,opts.split).options_status.should == ({:d => ["82096111"], :n => false, :P => "", :facsimile => ["test.ps"]})
   end
 end
